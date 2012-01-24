a710c1b14d657429448a31e51c6dd5ea69b5a11e
Ryan Sonnek <ryan@socialcast.com>
2012-01-24 14:22:42 -0600

    fix deprecation warning

--- lib/callback_skipper.rb
+++ lib/callback_skipper.rb
@@ -2,12 +2,10 @@ require "callback_skipper/version"
 
 module CallbackSkipper
   extend ActiveSupport::Concern
-  module InstanceMethods
-    def skip_callback(*args)
-      instance = self
-      args << {:if => lambda { self == instance }}
-      self.class.skip_callback *args
-    end
+  def skip_callback(*args)
+    instance = self
+    args << {:if => lambda { self == instance }}
+    self.class.skip_callback *args
   end
 end
 ActiveRecord::Base.send :include, CallbackSkipper
